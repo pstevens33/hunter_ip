e8459c0a374423cf8fe8913ba0561d57b588f2af
Parker Stevens <pstevens33@gmail.com>
2017-07-08 21:49:04 -0600

    Initial commit

---
+++ flask_app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, send_from_directory, jsonify, Response, g, flash, redirect
+from flask import make_response
+import os
+import json
+import requests
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/result', methods=['POST'])
+def result():
+    ip_form = request.form['ip_form']
+    return render_template('result.html', data=[get_ip_location(ip_form)])
+    
+@app.route('/get_ip_location')
+def get_ip_location(ip):
+    with open('../wut.txt') as filename:
+        key = filename.read()
+    return requests.get('https://context.skyhookwireless.com/accelerator/ip?version=2.0&ip={}&prettyPrint=true&key={} &user=eval'.format(ip,key)).content
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
